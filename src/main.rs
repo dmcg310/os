a1187368c881c14a237142e2561e6936a824f436
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-12-22 23:57:31 +0000

    feat: no harness tests

--- src/main.rs
+++ src/main.rs
@@ -30,3 +30,8 @@ fn panic(info: &PanicInfo) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     os::test_panic_handler(info)
 }
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,28 +1,21 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use os::{exit_qemu, serial_print, serial_println, QemuExitCode};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[TEST DID NOT PANIC]");
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-
-    for test in tests {
-        test();
-        serial_println!("[TEST DID NOT PANIC]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
+fn should_fail() {
+    serial_print!("[TEST] 'should_panic::should_fail' \t");
+    assert_eq!(0, 1);
 }
 
 #[panic_handler]
@@ -30,10 +23,4 @@ fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[OK]");
     exit_qemu(QemuExitCode::Success);
     loop {}
-}
-
-#[test_case]
-fn should_fail() {
-    serial_print!("[TEST] 'should_panic::should_fail' \t");
-    assert_eq!(0, 1);
 }
